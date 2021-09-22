ecfd5d2248c813196a80fe714eb9c06b4d0321b1
Hungyiu Wong <hungyiuwong@Hungyius-MacBook-Air.local>
2021-09-22 17:48:45 -0400

    adding to product-routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -7,12 +7,25 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  Product.findAll({
+    includes: [Category, Tag],
+  })
+  .then ((product) => res,json(product))
+  .catch ((err) => res.status(500).json(err))
 });
 
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  Product.findOne({
+    where: {
+      id: req.params.id
+    },
+    include: [Product]
+  })
+  .then((product) => res.json(product))
+  .catch((err) => res.status(400).json(err))
 });
 
 // create new product
